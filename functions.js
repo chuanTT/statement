7476416d01e105c5d7e59b5f1c04dc9394ceac90
ChuanTT <chuan123789@gmail.com>
2024-09-19 00:15:09 +0700

    init app

---
+++ functions.js
@@ -0,0 +1,182 @@
+import moment from "moment";
+
+/**
+ * @function
+ * @param {string} str
+ * @param {number} s
+ * @param {number | undefined} e
+ * @returns {string}
+ */
+export const sliceTrimValue = (str, s, e) => str.slice(s, e)?.trim();
+
+/**
+ * @function
+ * @param {string | undefined} value
+ * @param {string[]} arrCheck
+ * @returns {string}
+ */
+export const sliceDetailBank = (
+  value,
+  arrCheck = ["GIAO DỊCH NGÀY", "tài khoản:", "tài khoản"]
+) => {
+  if (!value) return;
+  const upperValue = value.toLocaleUpperCase();
+  const lowerValue = value.toLocaleLowerCase();
+  for (const key of arrCheck) {
+    const indexUpper = upperValue.indexOf(key);
+    const indexLower = lowerValue.indexOf(key);
+
+    if (indexUpper !== -1) {
+      return sliceTrimValue(value, indexUpper + key.length);
+    } else if (indexLower !== -1) {
+      return sliceTrimValue(value, indexLower + key.length);
+    }
+  }
+  return value;
+};
+
+/**
+ * @function
+ * @param {string | number} number
+ * @returns {number}
+ */
+export const convertNumber = (number) => {
+  if (!number || typeof number === "number") {
+    return number || 0;
+  }
+  number = number.replace(/[.]/g, "");
+  number = number.trim();
+  return +number;
+};
+
+/**
+ * @function
+ * @param {string | Date} date
+ * @param {string } format
+ * @returns {number}
+ */
+export const formatDate = (date, format = "YYYY-MM-DD") =>
+  moment(date, "DD/M/YYYY").format(format);
+
+/**
+ * @function
+ * @param {string | undefined} str
+ * @param {number} loop
+ * @param {string} character
+ * @returns {string}
+ */
+export const loopSliceValue = (str, loop, character = ".") => {
+  for (let i = 0; i < loop; i++) {
+    const indexNew = str.indexOf(character);
+    const isEnd = i === loop - 1;
+    str = str.slice(
+      isEnd ? 0 : indexNew + character?.length,
+      isEnd ? indexNew : undefined
+    );
+  }
+  return str;
+};
+
+/**
+ * @function returnValue
+ * @param {string} str - Chuỗi đầu vào
+ * @returns {string} - Chuỗi đầu ra
+ */
+
+/**
+ * @typedef {Object} BankDetails
+ * @property {string | undefined} keyBank
+ * @property {number | undefined} [loop]
+ * @property {string | undefined} [character]
+ * @property {string | undefined} [loopTransfer]
+ * @property {function(string): string} [func]
+ */
+
+/**
+ * @typedef {Object.<string, BankDetails>} TransactionBank
+ */
+
+/** @type {TransactionBank} */
+const objTransactionBank = {
+  VCB: {
+    keyBank: "MBVCB.",
+    loop: 1,
+  },
+  PARTNER: {
+    keyBank: "PARTNER.DIRECT_DEBITS_VCB.",
+    loop: 2,
+    loopTransfer: 3,
+    func: (value) => {
+      console.log(value);
+    },
+  },
+  OTHER: {
+    loop: 1,
+    loopTransfer: 3,
+  },
+};
+
+/**
+ * @function
+ * @param {string | undefined} transfer
+ * @returns {string}
+ */
+export const transactionNumberFunc = (transfer) => {
+  if (!transfer) return "";
+
+  for (const key in objTransactionBank) {
+    const { keyBank, loop, character } = objTransactionBank[key];
+    const indexStart = keyBank ? transfer?.indexOf(keyBank) : 0;
+    if (indexStart === -1) {
+      continue;
+    }
+    const newStr = keyBank
+      ? transfer?.slice(indexStart + keyBank?.length)
+      : transfer;
+
+    return loop === 0 || loop === undefined
+      ? newStr
+      : loopSliceValue(newStr, loop, character);
+  }
+};
+
+/**
+ * @function
+ * @param {string | undefined} str
+ * @param {number} loop
+ * @param {string} character
+ * @returns {string}
+ */
+export const loopSliceValueTransfer = (str, loop, character = ".") => {
+  for (let i = 0; i < loop; i++) {
+    const indexNew = str.indexOf(character);
+    str = str.slice(indexNew + 1);
+  }
+  return str;
+};
+
+/**
+ * @function
+ * @param {string | undefined} transfer
+ * @returns {string}
+ */
+
+export const transferContentFunc = (transfer) => {
+  if (!transfer) return "";
+  for (const key in objTransactionBank) {
+    const { keyBank, loop, loopTransfer, character, func } =
+      objTransactionBank[key];
+    const indexStart = keyBank ? transfer?.indexOf(keyBank) : 0;
+    if (indexStart === -1) {
+      continue;
+    }
+    const newLoop = loopTransfer ?? loop;
+    const newStr = keyBank
+      ? transfer?.slice(indexStart + keyBank?.length)
+      : transfer;
+    const newTransfer = loopSliceValueTransfer(newStr, newLoop, character);
+    console.log(loopSliceValueTransfer(newStr, newLoop, character));
+    return newTransfer;
+  }
+  return transfer;
+};
---
+++ index.js
@@ -0,0 +1,61 @@
+import fs from "fs";
+import { PdfDataReader } from "pdf-data-parser";
+import {
+  convertNumber,
+  formatDate,
+  sliceDetailBank,
+  transactionNumberFunc,
+  transferContentFunc,
+} from "./functions.js";
+
+transferContentFunc(
+  "PARTNER.DIRECT_DEBITS_VCB.MSE.66356036291.20240908.66356036291-0982077748_DANG NHA NGUYEN UNG HO KHAC PHUC HAU QUA BAO S3 YAGI"
+);
+// (async () => {
+//   const reader = new PdfDataReader({
+//     url: "./statement-14-09-2024.pdf",
+//     format: "json",
+//     pages: [1],
+//     repeatingHeaders: true,
+//   });
+//   const rows = [];
+//   const obj = {
+//     bankName: "",
+//     transactionDate: "",
+//     accountNumber: "",
+//   };
+//   const arrKeys = Object.keys(obj);
+//   let i = 0;
+
+//   reader.on("data", (row) => {
+//     const length = row?.length;
+//     if (length === 1) {
+//       const key = arrKeys?.[i];
+//       if (key) {
+//         obj[key] = sliceDetailBank(row?.[0]);
+//       }
+//     } else if (length === 4 && !row?.includes("STT")) {
+//       const transferContent = row?.[3]?.replace(/"/g, "")?.trim();
+//       rows.push({
+//         ...obj,
+//         transactionDate: row?.[1] ?? formatDate(obj?.transactionDate),
+//         amount: convertNumber(row?.[2]),
+//         transferContent: transferContentFunc(transferContent),
+//         transactionNumber: transactionNumberFunc(transferContent),
+//       });
+//     }
+//     i++;
+//   });
+
+//   reader.on("end", () => {
+//     fs.writeFileSync(
+//       `${formatDate(obj.transactionDate)}.json`,
+//       JSON.stringify(rows)
+//     );
+//     // do something with the rows
+//   });
+
+//   reader.on("error", (err) => {
+//     // log error
+//   });
+// })();
